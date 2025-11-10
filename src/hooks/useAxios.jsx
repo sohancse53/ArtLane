6f64626116c6b804b41ec2166fd2b33b8b111f2f
Md. Shoyaif Rahman <sohan0020p@gmail.com>
2025-11-11 02:06:59 +0600

    secure axios interceptor added

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,9 +1,38 @@
 import axios from "axios";
+import { use, useEffect } from "react";
+import Authcontext from "../context/Authcontext";
+import { useNavigate } from "react-router";
 
 const axiosInstance = axios.create({
     baseURL:'http://localhost:3000'
 });
 const useAxios = ()=>{
+    const navigate = useNavigate();
+    const {user,logOut} = use(Authcontext);
+    // request 
+   useEffect(()=>{
+    // request  interceptor
+    const requestInterceptor=  axiosInstance.interceptors.request.use((config)=>{
+        console.log(config);
+        config.headers.authorization = `Bearer ${user.accessToken}`;
+        return config
+    });
+    // response interceptor
+    const responseInterceptor = axiosInstance.interceptors.response.use((res)=>{return res} , (error)=>{
+        const status = error.status;
+        if(status ===401 || status === 403){
+            logOut()
+            navigate('/login');
+        }
+    })
+
+    return ()=>{
+        axiosInstance.interceptors.request.eject(requestInterceptor);
+        axiosInstance.interceptors.response.eject(responseInterceptor);
+    }
+
+   },[user,navigate,logOut])
+   
     return axiosInstance;
 }
 export default useAxios;
